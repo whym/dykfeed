92870f5eeb6b927d3c764b44fee8ee45bf407e2b
Yusuke Matsubara <whym@whym.org>
2026-08-25 19:08:58 +0900

    Tweak timing

--- run_rss.sh
+++ run_rss.sh
@@ -1,2 +1,3 @@
 #! /bin/bash -x
-sleep $(((RANDOM % 7) * 60))m ; ~/pyvenv/bin/python ~/dykfeed.py --output ~/public_html/rss.xml
+date
+sleep $(((RANDOM % 3) * 60))m ; ~/pyvenv/bin/python ~/dykfeed.py --output ~/public_html/rss.xml
